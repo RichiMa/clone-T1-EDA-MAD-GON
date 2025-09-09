db41ce85611321539a169319250f614be9317ef1
RichiMa <rimadariaga@miuandes.cl>
2025-09-09 02:06:59 +0000

    Entrega

---
+++ clustering/cpp/build/CMakeFiles/test.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test.dir/src/cluster.cpp.o"
+  "CMakeFiles/test.dir/src/cluster.cpp.o.d"
+  "CMakeFiles/test.dir/src/matrix.cpp.o"
+  "CMakeFiles/test.dir/src/matrix.cpp.o.d"
+  "CMakeFiles/test.dir/src/simsearch.cpp.o"
+  "CMakeFiles/test.dir/src/simsearch.cpp.o.d"
+  "CMakeFiles/test.dir/src/utils.cpp.o"
+  "CMakeFiles/test.dir/src/utils.cpp.o.d"
+  "CMakeFiles/test.dir/test.cpp.o"
+  "CMakeFiles/test.dir/test.cpp.o.d"
+  "test"
+  "test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
